6a08b1367433ce2e249474d6a873209e735fdbc5
chetan <cmehra890@gmail.com>
2024-06-01 10:44:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailerNamespace
+{
+	public class MailMainClass
+	{
+		static void Main(string[] args)
+		{
+			var smtpClient = new SmtpClient()
+			{
+				Host = "smtp.gmail.com",
+				Port = 587,
+				DeliveryMethod = SmtpDeliveryMethod.Network,
+				UseDefaultCredentials = false,
+				EnableSsl = true,
+				Credentials = new NetworkCredential(
+												Convert.ToString(ConfigurationManager.AppSettings["SERVER_MAIL_ADDRESS"]),
+												Convert.ToString(ConfigurationManager.AppSettings["SERVER_MAIL_PASSWORD"])
+												)
+			};
+			try
+			{
+				smtpClient.Send("dummy.chetan.mail@gmail.com", "cmehra890@gmail.com", "dummy test", "nothing special");
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	}
+}
